294ad4f955e1870e15cd3536d7625b0d56e8d2c0
Minh Dinh <ddm03@gmail.com>
2023-06-19 16:05:54 +0700

    19/06/2023: banner

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Banner from "./components/banner/Banner";
 import MovieList from "./components/movie/MovieList";
 
 function App() {
@@ -16,33 +17,7 @@ function App() {
         <span className="text-primary">Home</span>
         <span>Movie</span>
       </header>
-      <section className="banner h-[500px] page-container mb-20">
-        <div className="w-full h-full rounded-lg relative">
-          <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
-          <img
-            className="w-full h-full object-cover rounded-lg"
-            src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
-            alt=""
-          />
-          <div className="absolute left-5 bottom-5 w-full text-white">
-            <h2 className="font-bold text-3xl mb-5">Avengers: Endgame</h2>
-            <div className="flex items-center gap-x-3 mb-8">
-              <span className="py-2 px-4 border border-white rounded-md">
-                Adventure
-              </span>
-              <span className="py-2 px-4 border border-white rounded-md">
-                Action
-              </span>
-              <span className="py-2 px-4 border border-white rounded-md">
-                Adventure
-              </span>
-            </div>
-            <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">
-              Watch now
-            </button>
-          </div>
-        </div>
-      </section>
+      <Banner></Banner>
       <section className="movies-layout page-container pb-20">
         <h2 className="capitalize text-white mb-10 text-3xl font-bold">
           Now playing
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+import { fetcher } from "../../config";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const Banner = () => {
+  const [movies, setMovies] = useState([]);
+  const { data, error, isLoading } = useSWR(
+    "https://api.themoviedb.org/3/movie/upcoming?api_key=2a660acf57ccf68e13434b6e40b0adc0",
+    fetcher
+  );
+  useEffect(() => {
+    if (data && data.results) setMovies(data?.results);
+  }, [data]);
+
+  return (
+    <>
+      <section className="banner h-[600px] page-container mb-20">
+        <Swiper
+          grabCursor={"true"}
+          spaceBetween={40}
+          slidesPerView={"auto"}
+          className="swiper"
+        >
+          {movies &&
+            movies.length > 0 &&
+            movies.map((movie) => (
+              <SwiperSlide key={movie.id}>
+                <BannerItem movie={movie}></BannerItem>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </section>
+    </>
+  );
+};
+
+const BannerItem = (props) => {
+  const { poster_path, title } = props.movie;
+  return (
+    <div className="w-full h-full rounded-lg relative">
+      <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
+      <img
+        className="w-full h-full object-cover rounded-lg"
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        alt=""
+      />
+      <div className="absolute left-5 bottom-5 w-full text-white">
+        <h2 className="font-bold text-3xl mb-5">{title}</h2>
+        <div className="flex items-center gap-x-3 mb-8">
+          <span className="py-2 px-4 border border-white rounded-md">
+            Adventure
+          </span>
+          <span className="py-2 px-4 border border-white rounded-md">
+            Action
+          </span>
+          <span className="py-2 px-4 border border-white rounded-md">
+            Adventure
+          </span>
+        </div>
+        <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">
+          Watch now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
